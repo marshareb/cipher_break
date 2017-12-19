1a4435d056fa16940f4d6d161e288a0f3370f418
Drake's Desktop <DrakeDWhite@gmail.com>
2017-12-18 21:37:01 -0500

    just made some changes

--- cipherfun.py
+++ cipherfun.py
@@ -7,16 +7,10 @@ ltrs = list(string.ascii_lowercase)
 # MAIN FUNCTION
 def apply_cipher(cipher_dna, sentence):
 
-    # DNA IS OF THE FORM "ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    # DNA IS OF THE FORM "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     # CREATES A HASH TABLE FOR THE DNA CIPHER
     def make_dictionary(cipher_dna):
         return {cipher_dna[i] : ltrs[i] for i in range(len(ltrs))}
-
-    # TAKES INPUT OF LIST OF WORDS, OUTPUTS A SENTENCE
-    def make_sentence(ls):
-        x = str(ls[0])
-        for i in range(1, len(ls)): x += ' ' + str(ls[i])
-        return x
 
     # APPLIES DICTIONARY
     def apply_dictionary(cipher_dna, sentence):
@@ -24,8 +18,16 @@ def apply_cipher(cipher_dna, sentence):
         items = make_dictionary(cipher_dna)
         return list(map(lambda x: ''.join(x), list(map(lambda x: list(map(lambda y: items[y], x)), sentence.split()))))
 
+    # TAKES INPUT OF LIST OF WORDS, OUTPUTS A SENTENCE
+    def make_sentence(ls):
+        x = str(ls[0])
+        for i in range(1, len(ls)): x += ' ' + str(ls[i])
+        return x
+
+
+
     # RETURN STRING OF THE SENTENCE AFTER APPLYING THE CIPHER
     return make_sentence(apply_dictionary(cipher_dna, sentence))
 
 if __name__ == "__main__":
-    print(apply_cipher("zabcdefghijklmnopqrstuvwxyz", "Hello there"))
+    print(apply_cipher("zabcdefghijklmnopqrstuvwxy", "Hello there"))
