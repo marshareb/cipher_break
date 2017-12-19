10270017eee6ed7cb1c2aa4d79c164763b8f429d
marshareb <jamesmarshallreber@gmail.com>
2017-12-18 21:51:31 -0500

    Added errors for bad values.

--- cipherfun.py
+++ cipherfun.py
@@ -10,10 +10,14 @@ def apply_cipher(cipher_dna, sentence):
     # DNA IS OF THE FORM "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     # CREATES A HASH TABLE FOR THE DNA CIPHER
     def make_dictionary(cipher_dna):
+        if len(set(cipher_dna)) != 26 or len(cipher_dna) != 26:
+            raise ValueError("Cipher DNA must be 26 letters.")
         return {cipher_dna[i] : ltrs[i] for i in range(len(ltrs))}
 
     # APPLIES DICTIONARY
     def apply_dictionary(cipher_dna, sentence):
+        if len(sentence) == 0:
+            raise ValueError("Sentence cannot be empty.")
         sentence = sentence.lower()
         items = make_dictionary(cipher_dna)
         return list(map(lambda x: ''.join(x), list(map(lambda x: list(map(lambda y: items[y], x)), sentence.split()))))
@@ -24,10 +28,8 @@ def apply_cipher(cipher_dna, sentence):
         for i in range(1, len(ls)): x += ' ' + str(ls[i])
         return x
 
-
-
     # RETURN STRING OF THE SENTENCE AFTER APPLYING THE CIPHER
     return make_sentence(apply_dictionary(cipher_dna, sentence))
 
 if __name__ == "__main__":
-    print(apply_cipher("zabcdefghijklmnopqrstuvwxy", "Hello there"))
+    print(apply_cipher("zabcdefghijklmnopqrstuvwxy", "gdkkn sgdqd"))
